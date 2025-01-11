d19bb6e310fe23e3f7b446d13902bd2ad20ca41e
rifrifqyqy <rifqyh22@gmail.com>
2025-01-11 15:46:44 +0700

    feat(data): add data for skills

---
+++ src/data/Skills.ts
@@ -0,0 +1,141 @@
+export const designSkills = [
+	{
+		id: 1,
+		title: 'Figma',
+		thumbnail: '/images/sketch/figma.svg'
+	},
+	{
+		id: 2,
+		title: 'Adobe Illustrator',
+		thumbnail: '/images/sketch/illustrator.svg'
+	},
+	{
+		id: 3,
+		title: 'Adobe Photoshop',
+		thumbnail: '/images/sketch/photoshop.svg'
+	}
+];
+
+export const frontEndSkills = [
+	{
+		id: 1,
+		title: 'Javascript',
+		thumbnail: '/images/sketch/js.svg'
+	},
+	{
+		id: 2,
+		title: 'CSS',
+		thumbnail: '/images/sketch/css.svg'
+	},
+	{
+		id: 3,
+		title: 'TailwindCSS',
+		thumbnail: '/images/sketch/tailwind.svg'
+	},
+  {
+    id: 4,
+    title: 'Bootstrap',
+    thumbnail: '/images/sketch/bootstrap.svg'
+  },
+  {
+    id: 5,
+    title: 'Shadcn UI',
+    thumbnail: '/images/sketch/shadcn.svg'
+  },
+  {
+    id: 6,
+    title: 'React',
+    thumbnail: '/images/sketch/react.svg'
+  },
+  {
+    id: 7,
+    title: 'Next.js',
+    thumbnail: '/images/sketch/next.svg'
+  },
+  {
+    id: 8,
+    title: 'Svelte',
+    thumbnail: '/images/sketch/svelte.svg'
+  },
+  {
+    id: 9,
+    title: 'Vue JS',
+    thumbnail: '/images/sketch/vue.svg'
+  },
+  {
+    id: 10,
+    title: 'Nuxt JS',
+    thumbnail: '/images/sketch/nuxt.svg'
+  },
+  {
+    id: 11,
+    title: 'Remix JS',
+    thumbnail: '/images/sketch/remixjs.png'
+  },
+  {
+    id: 12,
+    title: 'Framer Motion',
+    thumbnail: '/images/sketch/framermotion.svg'
+  },
+  {
+    id: 13,
+    title: 'GSAP',
+    thumbnail: '/images/sketch/gsap2.svg'
+  }
+];
+
+export const backEndSkills = [
+	{
+		id: 1,
+		title: 'Node.js',
+		thumbnail: '/images/sketch/nodejs.svg'
+	},
+  {
+    id: 2,
+    title: 'Express',
+    thumbnail: '/images/sketch/express.svg'
+  },
+	{
+		id: 3,
+		title: 'PHP',
+		thumbnail: '/images/sketch/php.svg'
+	},
+  
+	{
+		id: 4,
+		title: 'Ruby',
+		thumbnail: '/images/sketch/ruby.svg'
+	},
+  {
+    id: 5,
+    title: 'Ruby on Rails',
+    thumbnail: '/images/sketch/rails.svg'
+  }
+];
+
+export const databaseSkills = [
+	{
+		id: 1,
+		title: 'MongoDB',
+		thumbnail: '/images/sketch/mongodb.svg'
+	},
+	{
+		id: 2,
+		title: 'mysql',
+		thumbnail: '/images/sketch/sql.svg'
+	}
+];
+
+export const otherSkills = [
+	{
+		id: 1,
+		title: 'Vite',
+		thumbnail: '/images/sketch/vitejs.png'
+	},
+
+	{
+		id: 3,
+		title: 'Trello',
+		thumbnail: '/images/sketch/trello.svg'
+	}
+];
