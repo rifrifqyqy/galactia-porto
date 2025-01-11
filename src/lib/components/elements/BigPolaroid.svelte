18fcedd2087cfadca73ac0af01bbe571e5650102
rifrifqyqy <rifqyh22@gmail.com>
2025-01-11 15:49:20 +0700

    feat(component): create card size big Polaroid

---
+++ src/lib/components/elements/BigPolaroid.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let portrait: string;
+</script>
+
+<main class="h-fit w-fit border-2 bg-white px-4 pb-16 pt-8 shadow-md">
+	<img src={portrait} class="aspect-auto w-[250px]" alt="" />
+</main>
+
+<style></style>
