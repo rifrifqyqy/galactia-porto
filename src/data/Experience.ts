1ff79713d8c21f1fedfae312143425945d38bc39
rifrifqyqy <rifqyh22@gmail.com>
2025-01-11 15:46:25 +0700

    feat(data): add experience data json

---
+++ src/data/Experience.ts
@@ -0,0 +1,44 @@
+export const experienceData = [
+	{
+		id: 1,
+		title: 'MSIB Alterra Academy',
+		thumbnail: '/images/alterra.png',
+		role: 'UIUX Designer',
+		date: 'August 2023',
+		duration: '5 months'
+	},
+	{
+		id: 2,
+		title: 'MSIB Maxy Academy',
+		thumbnail:
+			'https://yt3.googleusercontent.com/SpD5p-8fC-895sMm8jnVPMlf-JL0qAx29eTj4UWixCvMpJ2gXJL2EVW4cYdjpThhnp17Ow5q=s900-c-k-c0x00ffffff-no-rj',
+		role: 'Frontend Developer',
+		date: 'April 2024',
+		duration: '5 months'
+	},
+	{
+		id: 3,
+		title: 'Internship DConsulting',
+		thumbnail: '/images/dconsulting.png',
+		role: 'Frontend Developer',
+		date: 'May 2024',
+		duration: '3 months'
+	},
+	{
+		id: 4,
+		title: 'Member GDSC Ubhara',
+		thumbnail: '/images/gdsc.png',
+		role: 'Frontend Developer',
+		date: null,
+		duration: null
+	},
+	{
+		id: 5,
+		title: 'Digitalent Scholarship',
+		thumbnail:
+			'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRExLxoovw4FXfV7-SAjKmE1bPOtCqz-9TUbw&s',
+		role: 'Junior Web Developer',
+		date: 'August 2024',
+		duration: '1 months'
+	}
+];
